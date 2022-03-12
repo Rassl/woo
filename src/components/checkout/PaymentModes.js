f2f9fc95a4390ac2dd4af686d721e37df85f65b9
Rasul Sahibjanli <rsahibjanli@solit-clouds.ru>
2022-03-12 22:22:40 +0300

    set up build

--- src/components/checkout/PaymentModes.js
+++ src/components/checkout/PaymentModes.js
@@ -7,35 +7,35 @@ const PaymentModes = ( { input, handleOnChange } ) => {
 	return (
 		<div className="mt-3">
 			<Error errors={ errors } fieldName={ 'paymentMethod' }/>
-			{/*Direct bank transfers*/}
-			<div className="form-check woo-next-payment-input-container mt-2">
-				<label className="form-check-label">
-					<input onChange={ handleOnChange } value="bacs" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'bacs' === paymentMethod}/>
-					<span className="woo-next-payment-content">Direct Bank Transfer</span>
-				</label>
-			</div>
-			{/*Pay with Paypal*/}
-			<div className="form-check woo-next-payment-input-container mt-2">
-				<label className="form-check-label">
-					<input onChange={ handleOnChange } value="paypal" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'paypal' === paymentMethod}/>
-					<span className="woo-next-payment-content">Pay with Paypal</span>
-				</label>
-			</div>
-			{/*Check Payments*/}
-			<div className="form-check woo-next-payment-input-container mt-2">
-				<label className="form-check-label">
-					<input onChange={ handleOnChange } value="cheque" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'cheque' === paymentMethod}/>
-					<span className="woo-next-payment-content">Check Payments</span>
-				</label>
-			</div>
-			{/*Pay with Stripe*/}
 			<div className="form-check woo-next-payment-input-container mt-2">
 				<label className="form-check-label">
 					<input onChange={ handleOnChange } value="cod" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'cod' === paymentMethod}/>
 					<span className="woo-next-payment-content">Cash on Delivery</span>
 				</label>
 			</div>
-			<div className="form-check woo-next-payment-input-container mt-2">
+			{/*Direct bank transfers*/}
+			{/* <div className="form-check woo-next-payment-input-container mt-2">
+				<label className="form-check-label">
+					<input onChange={ handleOnChange } value="bacs" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'bacs' === paymentMethod}/>
+					<span className="woo-next-payment-content">Direct Bank Transfer</span>
+				</label>
+			</div> */}
+			{/*Pay with Paypal*/}
+			{/* <div className="form-check woo-next-payment-input-container mt-2">
+				<label className="form-check-label">
+					<input onChange={ handleOnChange } value="paypal" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'paypal' === paymentMethod}/>
+					<span className="woo-next-payment-content">Pay with Paypal</span>
+				</label>
+			</div> */}
+			{/*Check Payments*/}
+			{/* <div className="form-check woo-next-payment-input-container mt-2">
+				<label className="form-check-label">
+					<input onChange={ handleOnChange } value="cheque" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'cheque' === paymentMethod}/>
+					<span className="woo-next-payment-content">Check Payments</span>
+				</label>
+			</div> */}
+			{/*Pay with Stripe*/}
+			{/* <div className="form-check woo-next-payment-input-container mt-2">
 				<label className="form-check-label">
 					<input onChange={ handleOnChange } value="jccpaymentgatewayredirect" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'jccpaymentgatewayredirect' === paymentMethod}/>
 					<span className="woo-next-payment-content">JCC</span>
@@ -52,7 +52,7 @@ const PaymentModes = ( { input, handleOnChange } ) => {
 					<input onChange={ handleOnChange } value="stripe-mode" className="form-check-input mr-3" name="paymentMethod" type="radio" checked={'stripe-mode' === paymentMethod}/>
 					<span className="woo-next-payment-content">Stripe</span>
 				</label>
-			</div>
+			</div> */}
 			{/*	Payment Instructions*/}
 			<div className="woo-next-checkout-payment-instructions mt-2">
 				Please send a check to Store Name, Store Street, Store Town, Store State / County, Store Postcode.
